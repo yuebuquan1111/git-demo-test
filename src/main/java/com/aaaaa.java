6c95987c1962bd1fe4f04dfa5ba0d9f527e6cadf
lh <532073957@qq.com>
2025-03-04 22:50:10 +0800

    qqqq

--- src/main/java/com/aaaaa.java
+++ src/main/java/com/aaaaa.java
@@ -4,5 +4,6 @@ public class aaaaa {
     public static void main(String[] args) {
         System.out.println("aaaa");
         System.out.println("git modify");
+        System.out.println("push- github");
     }
 }
